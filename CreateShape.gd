4d431218753e933a8c23c324f61871c8d77f8949
Peter Dønvang <peterdonvang@protonmail.com>
2024-10-12 14:56:21 +0200

    shape added w. gravity

---
+++ CreateShape.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+var sprites = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# this is a comment
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ flag_piece.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_mouse_entered() -> void:
+	print("i am hovering")
+
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("left_click"):
+		print("clicked")
