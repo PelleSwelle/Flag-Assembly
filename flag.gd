fadd265d57c7bef57182cf704363ea9ef490f824
PelleSwelle <peterdonvang@protonmail.com>
2025-01-31 23:35:49 +0100

    extract readFlagAsXML

--- flag.gd
+++ flag.gd
@@ -7,16 +7,7 @@ var parser = XMLParser.new()
 func _ready() -> void:
 	flagPieceNode = load("res://flagPiece.tscn")
 	# load_pieces('saintKittsAndNevis')
-	parser.open("res://Flags/saintKittsAndNevis/Flag_of_Saint_Kitts_and_Nevis.svg")
-
-	while parser.read() != ERR_FILE_EOF:
-		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
-			var node_name = parser.get_node_name()
-			var attributes_dict = {}
-			for idx in range(parser.get_attribute_count()):
-				attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
-			print("The ", node_name, " element has the following attributes: ", attributes_dict)
-			
+	readFlagAsXML('saintKittsAndNevis')	
 
 	# instantiate_flag()
 	
@@ -44,6 +35,20 @@ func load_pieces(countryName: String):
 			
 	else:
 		printerr("an error occured trying to access the path")
+
+func readFlagAsXML(flag: String):
+	parser.open("res://Flags/saintKittsAndNevis/Flag_of_Saint_Kitts_and_Nevis.svg")
+
+	var inc = 0
+	while parser.read() != ERR_FILE_EOF:
+		inc += 1
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
+			var node_name = parser.get_node_name()
+			var attributes_dict = {}
+			for idx in range(parser.get_attribute_count()):
+				var attribute_name = parser.get_attribute_name(idx)
+				attributes_dict[attribute_name] = parser.get_attribute_value(idx)
+			print("The ", node_name, " ", inc,  " element has the following attributes: ", attributes_dict)
 
 func instantiate_flag():
 	for piece in flagPieces:
